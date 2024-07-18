9e1fdaac5e332351023f5805bdf626f7cbb4931b
zensei~ <bhargavburgul@gmail.com>
2024-07-18 10:03:19 +0530

    Integrate backend API URL

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(
 	cors({
-		origin: process.env.FRONTEND_URL || "http://localhost:5173",
+		origin: process.env.FRONTEND_URL ,
 		credentials: true,
 	})
 );
