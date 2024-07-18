21d26c5c6b72c6c043496395e1bce58878d87709
zensei~ <bhargavburgul@gmail.com>
2024-07-18 11:28:42 +0530

    Upadated url for CORs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,33 +8,29 @@ import cors from "cors";
 config();
 const app = express();
 
-// Middlewares
+// Middleware
 app.use(
-    cors({
-        origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL2],
-        credentials: true,
-        // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-    })
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL2], 
+    credentials: true,
+  })
 );
-
-// Handle preflight requests
-app.options('*', cors());
 
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // Use Morgan only in development
-if (process.env.NODE_ENV !== 'production') {
-    app.use(morgan("dev"));
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   app.use(morgan("dev"));
+// }
 
 // Routes
 app.use("/api/v1", appRouter);
 
-// Error handling middleware (if you have one)
+// Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 export default app;
